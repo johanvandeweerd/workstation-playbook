b1934aa8898fece2adae132a78f8cd16c6194a6a
Johan Vandeweerd <johan@vandeweerd.be>
2021-12-18 19:38:10 +0100

    Missing curl package and signin address and email address for 1Password

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # Install Ansible and pip
 sudo sh -c 'apt update \
-    && sudo apt install software-properties-common \
+    && sudo apt install -y software-properties-common curl \
     && sudo add-apt-repository --yes --update ppa:ansible/ansible \
-    && sudo apt install ansible python3-pip --yes \
+    && sudo apt install -y ansible python3-pip \
     && sudo apt autoremove --yes'
 
 # Install psutil required by Ansible dconf module
@@ -18,7 +18,9 @@ sudo sh -c 'curl https://cache.agilebits.com/dist/1P/op/pkg/v1.12.3/op_linux_amd
     && unzip /tmp/op.zip -x op.sig -d /usr/local/bin \
     && rm /tmp/op.zip \
     && chmod 755 /usr/local/bin'
-eval $(op signin)
+read -p 'Enter the 1Password sign-in address: ' site
+read -p 'Enter your 1Password email address: ' email
+eval $(op signin $site $email)
 
 # Run Ansible playbook
 ansible-playbook main.yaml --ask-become-pass
